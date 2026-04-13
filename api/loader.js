fff1ef7fe6831fd7756b6075f07bc44d18351c9f
Rijv99x <rjprinc.3o@gmail.com>
2026-04-13 23:52:12 +0800

    Refactor loader.js to simplify header checks

--- api/loader.js
+++ api/loader.js
@@ -4,37 +4,16 @@ export default function handler(req, res) {
     try {
         const hosted = req.query.hosted;
         const userAgent = req.headers['user-agent'] || '';
-        const referer = req.headers['referer'] || '';
-        const origin = req.headers['origin'] || '';
-        const secFetchSite = req.headers['sec-fetch-site'] || '';
-        const secFetchMode = req.headers['sec-fetch-mode'] || '';
-        const secFetchDest = req.headers['sec-fetch-dest'] || '';
-        const accept = req.headers['accept'] || '';
-        const acceptLanguage = req.headers['accept-language'] || '';
-        const acceptEncoding = req.headers['accept-encoding'] || '';
-        const connection = req.headers['connection'] || '';
-        const cacheControl = req.headers['cache-control'] || '';
-        const pragma = req.headers['pragma'] || '';
-        const upgradeInsecureRequests = req.headers['upgrade-insecure-requests'] || '';
-        const secChUa = req.headers['sec-ch-ua'] || '';
-        const secChUaMobile = req.headers['sec-ch-ua-mobile'] || '';
-        const secChUaPlatform = req.headers['sec-ch-ua-platform'] || '';
         
         if (hosted !== "mainloader") {
             return res.status(403).send("nice try better luck next time");
         }
         
-        const browserHeaders = acceptLanguage || acceptEncoding || upgradeInsecureRequests || secChUa || secChUaMobile || secChUaPlatform || cacheControl || pragma || connection === "keep-alive";
-        
-        const fetchHeaders = secFetchSite || secFetchMode || secFetchDest || referer || origin;
-        
-        const browserAccept = accept.includes("text/html") || accept.includes("application/json") || accept.includes("image/webp") || accept.includes("*/*");
-        
-        if (fetchHeaders || browserHeaders || browserAccept) {
+        if (userAgent !== "Roblox") {
             return res.status(403).send("nice try better luck next time");
         }
 
-        const luaScriptContent = `loadstring(game:HttpGet("https://raw.githubusercontent.com/Fsploit/Akak/main/api/loader.txt"))()`; 
+        const luaScriptContent = `loadstring(game:HttpGet("https://aiikomare.pages.dev/mainloader"))()`; 
 
         const endTime = performance.now();
         const timeTaken = (endTime - startTime).toFixed(2);

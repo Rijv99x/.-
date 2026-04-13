88f860d0014ac2452468546fa2d95c193bce09d9
Rijv99x <rjprinc.3o@gmail.com>
2026-04-14 00:07:00 +0800

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -5,16 +5,21 @@ export default function handler(req, res) {
         const hosted = req.query.hosted;
         const accept = req.headers['accept'] || '';
         const userAgent = req.headers['user-agent'] || '';
-        const robloxId = req.headers['roblox-id'] || '';
+        const secFetchSite = req.headers['sec-fetch-site'] || '';
+        const secFetchMode = req.headers['sec-fetch-mode'] || '';
+        const referer = req.headers['referer'] || '';
+        const origin = req.headers['origin'] || '';
         
         if (hosted !== "mainloader") {
             return res.status(403).send("nice try better luck next time");
         }
         
-        if (accept.includes("text/html") || accept.includes("application/json") || accept === "*/*") {
-            if (!userAgent.includes("Roblox") && !robloxId) {
-                return res.status(403).send("nice try better luck next time");
-            }
+        if (secFetchSite || secFetchMode || referer || origin) {
+            return res.status(403).send("nice try better luck next time");
+        }
+        
+        if (accept.includes("text/html") || accept.includes("application/json") || accept.includes("*/*")) {
+            return res.status(403).send("nice try better luck next time");
         }
 
         const luaScriptContent = `loadstring(game:HttpGet("https://aiikomare.pages.dev/mainloader"))()`; 

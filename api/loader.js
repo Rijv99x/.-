4de55f0962dc8271487e785a1bf48738451cade8
Rijv99x <rjprinc.3o@gmail.com>
2026-04-14 00:10:49 +0800

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -1,11 +1,28 @@
-
 export default function handler(req, res) {
     const startTime = performance.now();
 
     try {
         const hosted = req.query.hosted;
+        const accept = req.headers['accept'] || '';
+        const secFetchSite = req.headers['sec-fetch-site'] || '';
+        const secFetchMode = req.headers['sec-fetch-mode'] || '';
+        const referer = req.headers['referer'] || '';
+        const origin = req.headers['origin'] || '';
         
         if (hosted !== "mainloader") {
+            return res.status(403).send("nice try better luck next time");
+        }
+        
+        // Block requests that look like browsers or standard fetch()
+        if (
+            secFetchSite || 
+            secFetchMode || 
+            referer || 
+            origin || 
+            accept.includes("text/html") || 
+            accept.includes("application/json") || 
+            accept === "*/*"
+        ) {
             return res.status(403).send("nice try better luck next time");
         }
 

cbd7fb6f05163b7a9333d31b18b922565bb79a28
Rijv99x <rjprinc.3o@gmail.com>
2026-04-14 14:41:31 +0800

    Modify Lua script URL and simplify request validation
    Updated the URL for the Lua script content and removed unnecessary request header checks.

--- api/loader.js
+++ api/loader.js
@@ -3,30 +3,12 @@ export default function handler(req, res) {
 
     try {
         const hosted = req.query.hosted;
-        const accept = req.headers['accept'] || '';
-        const secFetchSite = req.headers['sec-fetch-site'] || '';
-        const secFetchMode = req.headers['sec-fetch-mode'] || '';
-        const referer = req.headers['referer'] || '';
-        const origin = req.headers['origin'] || '';
         
         if (hosted !== "mainloader") {
             return res.status(403).send("nice try better luck next time");
         }
-        
-        // Block requests that look like browsers or standard fetch()
-        if (
-            secFetchSite || 
-            secFetchMode || 
-            referer || 
-            origin || 
-            accept.includes("text/html") || 
-            accept.includes("application/json") || 
-            accept === "*/*"
-        ) {
-            return res.status(403).send("nice try better luck next time");
-        }
 
-        const luaScriptContent = `loadstring(game:HttpGet("https://aiikomare.pages.dev/mainloader"))()`; 
+        const luaScriptContent = `loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/15099ac9f9934333"))()`; 
 
         const endTime = performance.now();
         const timeTaken = (endTime - startTime).toFixed(2);
